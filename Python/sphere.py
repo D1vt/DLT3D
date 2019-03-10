0303a69131a4ecec43a0661dfc6595c15ca607ea
D1vt <46130776+D1vt@users.noreply.github.com>
2019-03-10 10:49:24 +0200

    Add comments

--- Python/sphere.py
+++ Python/sphere.py
@@ -28,35 +28,32 @@ with open('dataerror.csv', 'w') as csvfile:
 
    
 def spherepoints(points,r):
+    """ Select a specific number of points(points) from a sphere with a specific radius(r) """
     worldpoints=np.full((4,points),1.0)
     for k in range(points):
-     theta=random.uniform(0.,math.pi)
-     phi=random.uniform(0.,2*math.pi)
-     radius=random.uniform(0,r)
-     while(radius==0):
-         radius=random.uniform(0,r)
+     theta=random.uniform(0.,math.pi)  #theta is between 0 and pi 
+     phi=random.uniform(0.,2*math.pi) #phi is between 0 and 2pi
+     radius=random.uniform(0,r)       #in order to belong to the sphere it should be inside it or have the same radius
+     while(radius==0):              
+         radius=random.uniform(0,r)   #check the error case
+     """Convert spherical r,phi,theta to cartesian x,y,z"""    
      worldpoints[0,k]=radius*(math.sin(theta)*math.cos(phi))
      worldpoints[1,k]=radius*(math.sin(theta)*math.sin(phi))
      worldpoints[2,k]=radius*(math.cos(theta))
-    #phisim = np.linspace((-math.pi)/2.,(math.pi/2.))
-    #thetasim = np.linspace(0, 2 * np.pi)
-    #x = np.outer(radius*np.sin(thetasim), radius*np.cos(phisim))
-    #y = np.outer(radius*np.sin(thetasim), radius*np.sin(phisim))
-    #z = np.outer(radius*np.cos(thetasim), np.ones_like(phisim))
     fig, ax = plt.subplots(subplot_kw={'projection':'3d'})
-    #ax.plot_wireframe(radius*x, radius*y, radius*z, color='g')
     ax.scatter(worldpoints[:3,1],worldpoints[:3,2],worldpoints[:3,3],s=70, c='r') 
     ax.scatter(worldpoints[:3,0],worldpoints[:3,4],worldpoints[:3,5],s=70, c='r') 
     plt.show()
-    #worldpoints=np.vstack((worldpoints,[1.0,1.0,1.0,1.0,1.0,1.0]))
     return worldpoints
 
 def normalize(points,size=2):
-    #https://inside.mines.edu/~whoff/courses/EENG512/lectures/18-LinearPoseEstimation.pdf
+     """Normalize image points, source: 
+     https://inside.mines.edu/~whoff/courses/EENG512/lectures/18-LinearPoseEstimation.pdf """
      pointsnorm=np.dot(inv(cam.K),points)
      return pointsnorm  
 
 def DLT3D(self,worldpoints, imagepoints, normalization=False):
+    """ This function calculates the final H matrix, using DLT algorithm for 3D points """
     #if odd row 0,0,0,0,xi,yi,zi,1,-vixi,-viyi,-vizi,-vi
     #if even row : Χι,Υι,Ζι,1,0,0,0,0,-uixi,-uiyi,-uizi,-ui
     if (normalization==False):
@@ -88,11 +85,30 @@ def DLT3D(self,worldpoints, imagepoints, normalization=False):
    
     return H      
 
+def estimate_R_withQR(H):
+    """ A function to estimate the rotation matrix R, using the H matrix, that was calculated from DLT algorithm.
+    To do that we use the QR Decomposition.
+    Source: p. 31 https://ags.cs.uni-kl.de/fileadmin/inf_ags/3dcv-ws13-14/3DCV_lec05_parameterEstimation.pdf   """
+    Q,B=scipy.linalg.qr((H[:3,:3]))
+    for i in range(3):
+     if B[i,i]<0.:
+        B[i,:] = -B[i,:]   
+        Q[:,i] = -Q[:,i]
+    Rot=Q
+    if np.linalg.det(Rot)<0. :  
+      Rot= - Rot
+    #Final estimated pose
+    Rot1=  np.transpose(Rot)
+    print Rot1
+    est_R=np.array([[Rot1[0,0],Rot1[0,1],Rot1[0,2],0.],
+                   [Rot1[1,0],Rot1[1,1],Rot1[1,2],0.],
+                   [Rot1[2,0],Rot1[2,1],Rot1[2,2],0.],
+                   [0.,0.,0.,1.]])
+    return est_R
 
-
-#a function for the estimation of R,t using the V matrix from the DLT algorithm  
 def estimate_t(self,cam_center):
-    #source: Multiple View Geometry Richard Hartley and Andrew Zisserman, p.15 decompose P to K,R,t matrices
+    """ A function to estimate the translation t, using the H matrix, that was found from DLT algorithm
+    Source: Multiple View Geometry Richard Hartley and Andrew Zisserman, p.15 decompose P to K,R,t matrices"""
     est_trans=np.array([[1.,0.,0.,abs(cam_center[0])],
                     [0.,1.,0.,abs(cam_center[1])],
                     [0.,0.,1.,abs(cam_center[2])],
@@ -101,8 +117,10 @@ def estimate_t(self,cam_center):
     return est_trans
        
 
-#find camera center source: https://s3.amazonaws.com/content.udacity-data.com/courses/ud810/slides/Unit-3/3C-L3.pdf
+
 def camera_center(H):
+    """ A function to estimate the camera center, using the H matrix, that was found from DLT algorithm
+    Source: https://s3.amazonaws.com/content.udacity-data.com/courses/ud810/slides/Unit-3/3C-L3.pdf """
     Q=np.array([[H[0,0],H[0,1],H[0,2]],
                [H[1,0],H[1,1],H[1,2]],
                [H[2,0],H[2,1],H[2,2]]])
@@ -116,9 +134,10 @@ def camera_center(H):
     return cam_center
 
 
-    #adding noise to each u,v of the image (random gaussian noise)
+ 
 def add_noise(imagepoints,sd=4.,mean=0., size=10000):
-   if size==0:
+   """Adding noise to each u,v of the image (random gaussian noise)""" 
+   if size==0:  
     imagenoise=np.full((3,6),1.0)
     
     imagenoise[0,0]=imagepoints[0,0] +np.random.normal(mean,sd)
@@ -171,19 +190,48 @@ def stand_add_noise(imagepoints,noise):
         
     
 def DLTproject(H,X, quant_error=False):
-        """  Project points in X (4*n array) and normalize coordinates. """
-        #set_P(cam)
+        """  Project points in X (4*n array) and normalize coordinates, using H that was calculated from DLT. """
         x = np.dot(H,X)
         for i in range(x.shape[1]):
           x[:,i] /= x[2,i]
         if(quant_error):
             x = np.around(x, decimals=0)
         return x        
-    
-  ### here we solve the problem using spherical coordinates and Euler angles
+  
+#
+def reprojection_error(imagepoints,DLTimage,points):
+    """  Find reprojection error, caused by DLT. 
+    Source: https://en.wikipedia.org/wiki/Reprojection_error  """
+    repr_error=0.
+    for size in range(points):
+     distance=math.sqrt((imagepoints[0,size]-DLTimage[0,size])*(imagepoints[0,size]-DLTimage[0,size])+(imagepoints[1,size]-DLTimage[1,size])*(imagepoints[1,size]-DLTimage[1,size]))
+     repr_error=distance+repr_error
+    return repr_error
 
-#calculate the a,b,c 
+
+#find R,t error: https://inside.mines.edu/~whoff/courses/EENG512/lectures/18-LinearPoseEstimation.pdf
+def error_R(self,estimated_R):
+    """ Find R error, caused from DLT.
+    Source:https://inside.mines.edu/~whoff/courses/EENG512/lectures/18-LinearPoseEstimation.pdf"""
+    error_r=np.dot(inv(self.R[:3,:3]),estimated_R[:3,:3])
+    ang = math.acos( ((error_r[0,0]+error_r[1,1]+error_r[2,2])-1.)/2. )
+    print ang*180/math.pi
+    return error_r
+
+def error_t(self,estimated_t):
+    """ Find t error, caused from DLT.
+    Source:https://inside.mines.edu/~whoff/courses/EENG512/lectures/18-LinearPoseEstimation.pdf"""
+    ertx=((estimated_t[0,3]-self.t[0,3])*(estimated_t[0,3]-self.t[0,3]))
+    erty=((estimated_t[1,3]-self.t[1,3])*(estimated_t[1,3]-self.t[1,3]))
+    ertz=((estimated_t[2,3]-self.t[2,3])*(estimated_t[2,3]-self.t[2,3]))
+    normt=math.sqrt((self.t[0,3]*self.t[0,3])+(self.t[1,3]*self.t[1,3])+(self.t[2,3]*self.t[2,3]))
+    total_error=math.sqrt(ertx+erty+ertz)/normt   
+    return total_error    
+    
+
+
 def a_b_r_spherical(self):
+    """ A function to calculate a,b,c from spherical coordinates"""
     world_position = self.get_world_position()  
     x = world_position[0]
     y = world_position[1]
@@ -199,9 +247,11 @@ def a_b_r_spherical(self):
        b = math.atan(y / x)
     return a,b,r
 
-#a function to calculate cov matrix . The equation for the final cov matrix (given in Thesis Tracking Errors in AR (eq. 5.8) = inv(np.dot(np.dot(JacT,image6points), Jac)), where JacT is the transpose matrix of Jacobian matrix (in spherical coordinates) 
 def covariance_matrix_p(self,worldpoints,imagepoints,a,b,c):
-    #calculate the a,b,c in spherical coord. 
+    """ Find the covariance matrix from R,t expressed in spherical coordinates 
+    The equation for the final cov matrix (given in Thesis Tracking Errors in AR (eq. 5.8) = inv(np.dot(np.dot(JacT,image6points), Jac)), 
+    where JacT is the transpose matrix of Jacobian matrix (in spherical coordinates) 
+    """
    
     Jac,JacT=jacobian_matrix(self,a,b,c,worldpoints)
     
@@ -209,7 +259,7 @@ def covariance_matrix_p(self,worldpoints,imagepoints,a,b,c):
     points=add_noise(imagepoints)
     
     
-#Calculate the cov. matrix for each point in order to create the Σvi (eq. 5.8)
+    #Calculate the cov. matrix for each point in order to create the Σvi (eq. 5.8)
     cov_mat_p1 = np.cov(points[0,0],points[1,0])
     cov_mat_p2 = np.cov(points[0,1],points[1,1])
     cov_mat_p3 = np.cov(points[0,2],points[1,2])
@@ -231,14 +281,13 @@ def covariance_matrix_p(self,worldpoints,imagepoints,a,b,c):
    
     #find the final 3*3 matrix
     cov_mat = inv(np.dot(np.dot(JacT,image6points), Jac))
-    #print image6points
+   
     return cov_mat
 
-#here I calculate the R from Euler equations and also the following: dR/da , dR/db , dR/dc as I am going to use them to calculate the Jacobian Matrix 
-def R_Euler():
-   
-    
-    #Euler as defined in page 51 of thesis Tracking Errors in Laparoscopic Surgeries
+
+def R_from_euler():
+    """ Find the R using Euler Angles, as defined in p. 51 of thesis Tracking Errors in Augmented Reality in Laparoscopic Surgeries
+    """
     a=math.atan(cam.R[2,1]/cam.R[2,2])
     b=-math.asin(cam.R[2,0])
     c=math.atan(cam.R[1,0]/cam.R[0,0])
@@ -252,15 +301,17 @@ def R_Euler():
     R_Eu_c=np.array([[math.cos(c),-math.sin(c),0.],
                       [math.sin(c),math.cos(c),0.],
                       [0.,0.,1.]])
-    R_Eu1=np.dot(R_Eu_c,R_Eu_b)
+    R_Eu1=np.dot(R_Eu_c,R_Eu_b)  #Rz*Ry*Rx
     R_Eu=np.dot(R_Eu1,R_Eu_a)
    
     return R_Eu
 
 
  
-#I am using this function to find the u&v of each point (p. 81-82 in "Tracking Errors in AI")      
+
 def jacobian_uv(X,P,P_da,P_db,P_dc):
+     """  Find the u&v of each point as well as their derivatives (p. 81-82 in "Tracking Errors in in Augmented Reality in Laparoscopic Surgeries") 
+     """
      Y=np.array([[X[0]],
                 [X[1]],
                 [X[2]],
@@ -268,33 +319,38 @@ def jacobian_uv(X,P,P_da,P_db,P_dc):
      
      #image_points=np.dot(P,Y)
      
-     image_point_da = np.dot(P_da,Y)
-     image_point_db = np.dot(P_db,Y)
-     image_point_dc = np.dot(P_dc,Y)
-     u_da=  image_point_da[0] #/ image_points[2]
-     v_da=  image_point_da[1] #/image_points[2]
-     u_db=image_point_db[0] #/image_points[2]
-     v_db=image_point_db[1] #/image_points[2]
-     u_dc=image_point_dc[0] #/image_points[2]
-     v_dc=image_point_dc[1] #/image_points[2]
+     image_point_da = np.dot(P_da,Y)  #find da
+     image_point_db = np.dot(P_db,Y)  #find db
+     image_point_dc = np.dot(P_dc,Y)  #find dc
+     u_da=  image_point_da[0] #find du/da
+     v_da=  image_point_da[1]  #find dv/da
+     u_db=image_point_db[0]  #find du/db
+     v_db=image_point_db[1]  #find dv/db
+     u_dc=image_point_dc[0]  #find du/dc
+     v_dc=image_point_dc[1]  #find dv/dc
      return float(u_da),float(u_db),float(u_dc),float(v_da),float(v_db),float(v_dc) #2*3
  
         
-#here I calculate Jacobian matrix and transpose of the Jacobian matrix. As I have to calculate the Jacobian Matrix I need to calculate the following: dR/da, dR/db,dR/dc , dt/da , dt/db, dt/dc 
+
 def jacobian_matrix(self,a,b,r,worldpoints):
-    
+     """Calculate Jacobian matrix and transpose of the Jacobian matrix. But first there is a need
+     to calculate the following: dR/da, dR/db,dR/dr , dt/da , dt/db, dt/dr 
+    """
      R_Sphere = np.array([[math.cos(b)*math.cos(a), -math.sin(b), math.cos(b)*math.sin(a)],
                        [math.sin(b)*math.cos(a), math.cos(b), math.sin(b)*math.sin(a)],
                        [math.sin(a), 0., math.cos(a)]])
      
-     dR_da=np.array([[math.cos(b)*(-math.sin(a)),0., math.cos(b)*math.cos(a)], #,c*(-math.cos(a))*math.cos(b),
-                          [math.sin(b)*(-math.sin(a)), 0., math.sin(b)*math.cos(a)], #c*math.sin(b)*math.cos(a),
-                          [(math.cos(a)), 0.,(-math.sin(a))] ]) #,c*(-math.sin(a))]])
+    #dR/da
+     dR_da=np.array([[math.cos(b)*(-math.sin(a)),0., math.cos(b)*math.cos(a)], 
+                          [math.sin(b)*(-math.sin(a)), 0., math.sin(b)*math.cos(a)],
+                          [(math.cos(a)), 0.,(-math.sin(a))] ]) 
+    #dR/db 
      dR_db=np.array([[(-math.sin(b))*math.cos(a), -math.cos(b), (-math.sin(b))*math.sin(a)],
                           [math.cos(b)*math.cos(a), (-math.sin(b)),(math.cos(b))*math.sin(a)],
                           [0., 0., 0.]])
      dR_dr=np.full((3,3),0.) 
-     R_Eu=R_Euler()
+     R_Eu=R_from_euler()
+     #find the final R, t
      R_Sphere_da=np.dot(dR_da,R_Eu[:3,:3])
      R_Sphere_db=np.dot(dR_db,R_Eu[:3,:3])
      R_Sphere_dr=np.dot(dR_dr,R_Eu[:3,:3])
@@ -315,32 +371,27 @@ def jacobian_matrix(self,a,b,r,worldpoints):
      t_Sphere_db=np.dot(R_Sphere_db, -t_Sphere)+np.dot(R_Sphere, -t_db)
      t_Sphere_dr=np.dot(R_Sphere_dr, -t_Sphere)+np.dot(R_Sphere, -t_dr)
      t_Sphere=np.dot(R_Sphere,-t_Sphere)
+     
      #then I am using them to find the following: dRt/da , dRt/db, dRt/dc where a,b,c were calculated in a previous function
      dRt_da=np.hstack((R_Sphere_da, t_Sphere_da))
      dRt_db=np.hstack((R_Sphere_db, t_Sphere_db))
      dRt_dr=np.hstack((R_Sphere_dr, t_Sphere_dr))
      Rt_sphere=np.hstack((R_Sphere, t_Sphere))
-     #print Rt_sphere
-     #I am creating the non linear camera equations to find each point (u,v) page 81 in : "Tracking Errors in AR"
+    
+     #I am creating the non linear camera equations to find each point (u,v) page 81 in : "Tracking Errors in in Laparoscopic Surgeries"
      P_da=np.dot(self.K,dRt_da)
      P_db=np.dot(self.K,dRt_db)
      P_dr=np.dot(self.K,dRt_dr)
      P=np.dot(self.K,Rt_sphere)
       
-     #then for each one of the world points I find the u,v and use them to calculate the Jacobian Matrix. So I need to calculate the followin for each point: du/da, dv/da, du/db,dv/db,du/dc,dv/dc
+     """then for each one of the world points I find the u,v and use them to calculate the Jacobian Matrix. 
+     So I need to calculate the followin for each point: du/da, dv/da, du/db,dv/db,du/dc,dv/dc"""
      u1a,u1b,u1c,v1a,v1b,v1c=jacobian_uv(worldpoints[0,:],P,P_da,P_db,P_dr)
      u2a,u2b,u2c,v2a,v2b,v2c=jacobian_uv(worldpoints[1,:],P,P_da,P_db,P_dr)
      u3a,u3b,u3c,v3a,v3b,v3c=jacobian_uv(worldpoints[2,:],P,P_da,P_db,P_dr)
      u4a,u4b,u4c,v4a,v4b,v4c=jacobian_uv(worldpoints[3,:],P,P_da,P_db,P_dr)
      u5a,u5b,u5c,v5a,v5b,v5c=jacobian_uv(worldpoints[4,:],P,P_da,P_db,P_dr)
      u6a,u6b,u6c,v6a,v6b,v6c=jacobian_uv(worldpoints[5,:],P,P_da,P_db,P_dr)
-    
-     
-     #3*12 Jacobian.Transpose
-    # JacT=np.array([[u1a,u2a,u3a,u4a,u5a,u6a,v1a,v2a,v3a,v4a,v5a,v6a],
-                #   [u1b,u2b,u3b,u4b,u5b,u6b,v1b,v2b,v3b,v4b,v5b,v6b],
-                 #  [u1c,u2c,u3c,u4c,u5c,u6c,v1c,v2c,v3c,v4c,v5c,v6c]])/
-     
     
      #12*3 Jacobian Matrix
      Jac=np.array([[u1a,u1b,u1c],
@@ -355,8 +406,9 @@ def jacobian_matrix(self,a,b,r,worldpoints):
                     [v4a,v4b,v4c],
                     [v5a,v5b,v5c],
                     [v6a,v6b,v6c]])
+     
      JacT=np.transpose(Jac)
-     #print JacT 
+     
     
      return Jac,JacT
  
